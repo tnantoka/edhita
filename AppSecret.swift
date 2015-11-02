7a348c0365772e54fdf1f0180fce0b780ce85eb2
tnantoka <tnantoka@bornneet.com>
2015-11-02 13:01:06 +0900

    Add AppSecret.swift

---
+++ AppSecret.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class AppSecret: NSObject {
+    struct Ad {
+        struct AdMob {
+            static let unitId = "ca-app-pub-xxxxxxxxxxxxxxxx/nnnnnnnnnn"
+        }
+    }
+}
+
